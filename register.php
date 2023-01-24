d7db0dbcd87ea40b37d1b031176d2d76eca0b4be
hooverlds2020 <hooverlds93@gmail.com>
2023-01-24 07:22:14 -0600

    first commit

---
+++ register.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Register</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row vh-100 justify-content-around align-items-center">
+            <h1 class="text-center">Register</h1>
+
+            <div class="card" style="width: 28rem;">      
+                <div class=" mt-3 mb-2">
+                   <label>Name</label>
+                    <input type="text" class="form-control" placeholder="Enter your name...">
+                </div>
+                <div class=" mt-3 mb-2">
+                   <label>Bio</label>
+                    <input type="text" class="form-control" placeholder="Enter your name Bio...">
+                </div>
+                <div class=" mt-3 mb-2">
+                   <label>Phone</label>
+                    <input type="text" class="form-control" placeholder="Enter your phone...">
+                </div>
+                <div class=" mt-3 mb-2">
+                   <label>Email</label>
+                    <input type="email" class="form-control" placeholder="Enter email name@example.com">
+                </div>
+                <div class=" mt-3 mb-2">
+                    <label>Password</label>
+                    <input type="password" class="form-control" placeholder="Your password">
+                </div>
+                <div class=" mt-3 mb-2">
+                    <label>Avatar</label>
+                    <input type="password" class="form-control" placeholder="Your password">
+                </div>
+
+                <input type="submit" class="form-control btn btn-primary mt-2 mb-2" value="Register" />
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
